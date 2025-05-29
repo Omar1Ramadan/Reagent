4f223a138329bea974ff79f756650de2f5fff784
Omar1Ramadan <oramadan2004@gmail.com>
2025-05-29 02:53:27 -0400

    adding premake script for building project easier

---
+++ premake5.lua
@@ -0,0 +1,112 @@
+workspace "Reagent"
+    architecture "x64"
+    
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Reagent"
+    location "Reagent"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "%{prj.name}/etc/spdlog/include"
+    }
+
+    buildoptions { "/utf-8"}
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "RG_WINDOWS",
+            "RG_DLL"
+        }
+
+        postbuildcommands
+        {
+            -- Ensure target directory exists first
+            "mkdir ../bin/" .. outputdir .. "/Sandbox 2>nul",
+            -- Then copy the DLL file there
+            "{COPYFILE} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox/"
+        }
+
+    filter "configurations:Debug"
+        defines { "RG_DEBUG" }
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines { "RG_RELEASE" }
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines { "RG_DIST" }
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "Reagent/etc/spdlog/include",
+        "Reagent/src"
+    }
+
+    buildoptions { "/utf-8"}
+
+    filter "system:windows"
+        cppdialect "C++20"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "RG_WINDOWS"
+        }
+
+    links
+    {
+        "Reagent"
+    }
+
+    filter "configurations:Debug"
+        defines { "RG_DEBUG" }
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines { "RG_RELEASE" }
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines { "RG_DIST" }
+        optimize "On"
---
+++ premakergen.bat
@@ -0,0 +1,3 @@
+@echo off  
+call .\etc\bin\premake\premake5.exe vs2022  
+PAUSE
